cac78cbbe81d35eae67338082e9857f6bdbb111f
anuHait <anusmitahait67@gmail.com>
2024-01-29 19:21:36 +0530

    home page ui changes

--- frontend/app/(auth)/page.jsx
+++ frontend/app/(auth)/page.jsx
@@ -7,8 +7,8 @@ const Login = () => {
     <div>
     <Provider>
     <ToasterContext />
-    <div className='flex flex-row gap-5'>
-    <img src="/assets/cover.jpg" className='w-[55%] h-full'/>
+    <div className='flex flex-col lg:flex-row gap-5'>
+    <img src="/assets/cover.jpg" className='w-full h-[45%] lg:w-[52%] lg:h-full'/>
     <div className='flex  flex-col items-center justify-center'>
     <h1 className='text-3xl font-semibold text-center'>Welcome to ChatVista</h1>
       <Form type="login" />
--- frontend/app/(auth)/register/page.jsx
+++ frontend/app/(auth)/register/page.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Form from '@components/Form'
 const Register = () => {
   return (
-    <div className='flex flex-row gap-5'>
+    <div className='flex flex-col lg:flex-row gap-5'>
     
-    <img src="/assets/cover.jpg" className='w-[55%] h-full'/>
+    <img src="/assets/cover.jpg" className='w-full h-[45%] lg:w-[52%] lg:h-full'/>
     <div className='flex  flex-col items-center justify-center' >
     <h1 className='text-3xl font-semibold text-center'>Welcome to ChatVista</h1>
       <Form type="register" />
